0bf2c0bdee4464a21fedb92da1e164e116adf4ee
Szymon Debski <szymonsitno@gmail.com>
2023-06-02 22:16:14 +0200

    login/signup/base/home templates setup

--- website/__init__.py
+++ website/__init__.py
@@ -4,4 +4,14 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'asdagafsdfnjsndifusndjfsdnfskfasijfaijsofa'
     
+    
+    from .views import views
+    from .auth import auth
+    
+    
+    
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    
+    
     return app
