422d076a56fc5d0a63c7eb08c34a0cfc4daabbb2
Szymon Debski <szymonsitno@gmail.com>
2023-06-03 16:35:36 +0200

    user management, db management

--- website/__init__.py
+++ website/__init__.py
@@ -1,22 +1,38 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+from sqlalchemy_utils.functions import database_exists
+from flask_login import LoginManager
 
 db = SQLAlchemy()
-DB_NAME = 'database.db'
+db_name = 'database.db'
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'asdagafsdfnjsndifusndjfsdnfskfasijfaijsofa'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_name}'
     db.init_app(app)
+    
     
     from .views import views
     from .auth import auth
     
-    
-    
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+#     from .models import User
+    from .models import User, Session, Game
+
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
     
-    
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+            
+            
     return app
+
--- website/models.py
+++ website/models.py
@@ -1,14 +1,35 @@
 from . import db
 from flask_login import UserMixin
-from sqllachemy.sql import func
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
 
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
-    first_name = db.Columng(db.String(150))
+    first_name = db.Column(db.String(150))
+    sessions = relationship('Session', back_populates='user')
+
     
 class Session(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.DateTime(timezone=True), defualt=func.now())
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) 
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    points = db.Column(db.Integer, default=10)
+    finished = db.Column(db.Boolean, default=False)
+    user = relationship('User', back_populates='sessions')
+    
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    session1_id = db.Column(db.Integer, db.ForeignKey('session.id'))
+    user1_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    session2_id = db.Column(db.Integer, db.ForeignKey('session.id'), nullable = True)
+    user2_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = True)
+    result = db.Column(db.Integer, default = 0)
+    
+    session1 = relationship('Session', foreign_keys='session1_id')
+    user1 = relationship('User', foreign_keys='user1_id')
+    session1 = relationship('Session', foreign_keys='session2_id')
+    user2 = relationship('User', foreign_keys='user2_id')
+    
